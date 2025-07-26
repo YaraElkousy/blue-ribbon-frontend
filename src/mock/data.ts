c1757ad9b0cfe699eb39f1a065a778658928d78b
YaraElkousy <128088959+YaraElkousy@users.noreply.github.com>
2025-07-26 19:58:54 +0300

    folder structure and mock data

---
+++ src/mock/data.ts
@@ -0,0 +1,12 @@
+export const sports = [
+    { id: 1, name: "Football" },
+    { id: 2, name: "Basketball" }
+  ];
+  
+  export const members = [
+    { id: 1, name: "Ahmed" },
+    { id: 2, name: "Habiba" },
+    { id: 2, name: "Yara" },
+    { id: 2, name: "Mohamed" }
+  ];
+  
